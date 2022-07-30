b79ddd3b7cb5eb84c29a60d37a77fdc5444d6f3a
d3fau1t <aciddust20@gmail.com>
2022-07-30 22:53:27 +0900

    Fix env

--- serverless/modules/env.py
+++ serverless/modules/env.py
@@ -1,5 +1,4 @@
 import json
-from bson import json_util
 import pipes
 from os import getenv, environ, path
 
@@ -12,17 +11,21 @@ def init_json_env() -> None:
     try:
         secrets = path.join(path.dirname(path.dirname(__file__)), "config.json")
         with open(secrets, "r") as json_file:
-            for k, v in json.load(json_file).items():
-                k = pipes.quote(k)
-                v = pipes.quote(v)
+            # get env from json file
+            secrets = json.load(json_file)
+            for key, value in secrets.items():
+                if getenv(key) is None:
+                    # set env from json file
+                    environ[key] = str(value)
     except FileNotFoundError:
         return None
 
+
 init_json_env()
 
-REDIS = {
-    'host': getenv("REDIS_HOST"),
-    'port': getenv("REDIS_PORT"),
-    'user': getenv("REDIS_USER"),
-    'password': getenv("REDIS_PASSWORD"),
+REDIS_CONFIG = {
+    "host": getenv("REDIS_HOST"),
+    "port": getenv("REDIS_PORT"),
+    "user": getenv("REDIS_USER"),
+    "password": getenv("REDIS_PASSWORD"),
 }
