999915c2cc8ea3e6e1210639ff6074e75159975e
d3fau1t <aciddust20@gmail.com>
2022-07-31 12:22:52 +0900

    Update expire extend

--- serverless/services/get_url.py
+++ serverless/services/get_url.py
@@ -2,7 +2,7 @@ from modules import redis
 from typing import Tuple
 
 
-def get_id_by_param_name(event, param_name) -> Tuple[dict, int]:
+def get_id_by_param(event, param_name) -> Tuple[dict, int]:
     if not event.get(param_name):
         return ({"message": "Not Found"}, 404)
     if not event[param_name].get("id"):
@@ -13,6 +13,9 @@ def get_id_by_param_name(event, param_name) -> Tuple[dict, int]:
 
     if not origin_url:
         return ({"message": "No origin url"}, 404)
+
+    redis.extend_expire(original_url, days=7)
+    redis.extend_expire(short_id, days=7)
 
     body = {
         "short_id": short_id,
