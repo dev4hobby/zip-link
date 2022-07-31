594a82f791411fc70113fc9f25d613cc533f7ec6
d3fau1t <aciddust20@gmail.com>
2022-07-31 12:22:13 +0900

    Add url validator

--- serverless/modules/utils.py
+++ serverless/modules/utils.py
@@ -1,10 +1,20 @@
 import json
+import re
 from random import choice
 from string import ascii_letters, digits
 
 # Random string generator, limit 6
 def generate_random_string(length=6):
     return "".join(choice(ascii_letters + digits) for i in range(length))
+
+
+def validate_url_with_regex(url):
+    if not url:
+        return False
+    return re.match(
+        r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$",
+        url,
+    )
 
 
 def json_response(body, status_code=200):
