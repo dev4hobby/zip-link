08fa857a7743eaaa14c83cbd10d0d1272f9d795a
d3fau1t <aciddust20@gmail.com>
2022-07-31 13:11:18 +0900

    Update redirect

--- serverless/modules/utils.py
+++ serverless/modules/utils.py
@@ -4,11 +4,11 @@ from random import choice
 from string import ascii_letters, digits
 
 # Random string generator, limit 6
-def generate_random_string(length=6):
+def generate_random_string(length=6) -> str:
     return "".join(choice(ascii_letters + digits) for i in range(length))
 
 
-def validate_url_with_regex(url):
+def validate_url_with_regex(url) -> bool:
     if not url:
         return False
     return re.match(
@@ -17,8 +17,15 @@ def validate_url_with_regex(url):
     )
 
 
-def json_response(body, status_code=200):
+def json_response(body, status_code=200) -> dict:
     return {
         "statusCode": status_code,
         "body": json.dumps(body),
     }
+
+
+def update_headers(headers: dict, response: dict) -> dict:
+    if response.get("headers"):
+        if headers:
+            response.update("headers", headers)
+    return response
---
+++ serverless/services/redirect_url.py
@@ -0,0 +1,20 @@
+from modules import redis
+
+
+def redirect_url_by_param(event, param_name):
+    if not event.get(param_name):
+        return ({"message": "Not Found"}, 404)
+    if not event[param_name].get("id"):
+        return ({"message": "Short ID Not Found"}, 404)
+
+    short_id = event[param_name]["id"]
+    origin_url = redis.get(short_id)
+
+    if not origin_url:
+        return ({"message": "No origin url"}, 404)
+
+    headers = {"Location": origin_url}
+
+    body = {}
+
+    return headers, body, 301
