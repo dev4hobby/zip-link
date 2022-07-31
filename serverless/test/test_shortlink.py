b15ed9fa085918d66c6fab86d2ae55f7d5eee166
d3fau1t <aciddust20@gmail.com>
2022-07-31 09:17:50 +0900

    Add test

--- serverless/test/test_shortlink.py
+++ serverless/test/test_shortlink.py
@@ -1,2 +1,11 @@
+def test_short_string():
+    from modules.utils import generate_random_string
+
+    string_length = 6
+    generated_string = generate_random_string(6)
+    assert generate_random_string is not None
+    assert len(generated_string) == string_length
+
+
 def test_shortlink():
     assert True is True
