0ddeae06127c5719c9e3f78353bef497c84397d8
d3fau1t <aciddust20@gmail.com>
2022-07-31 09:15:39 +0900

    Add serverless get handler w/ queryParam, pathParam

--- serverless/handler.py
+++ serverless/handler.py
@@ -1,10 +1,16 @@
-import json
+from modules.utils import json_response
+from services.get_url import get_id_by_param_name
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+def get_url(event, context):
+    body, status_code = get_id_by_param_name(event, "queryStringParameters")
+    return json_response(body, status_code)
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+
+def get_url_by_pathparam(event, context):
+    body, status_code = get_id_by_param_name(event, "pathParameters")
+    return json_response(body, status_code)
+
+
+def set_url(event, context):
+    return json_response({"message": "Not Implemented"})
