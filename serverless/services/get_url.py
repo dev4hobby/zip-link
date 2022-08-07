b79455244e7175069a854449b02963906ebd6666
d3fau1t <aciddust20@gmail.com>
2022-08-07 20:30:41 +0900

    Fix, days -> ttl=timedelta

--- serverless/services/get_url.py
+++ serverless/services/get_url.py
@@ -1,6 +1,6 @@
 from modules import redis
 from typing import Tuple
-
+from datetime import timedelta
 
 def get_id_by_param(event, param_name) -> Tuple[dict, int]:
     if not event.get(param_name):
@@ -14,8 +14,8 @@ def get_id_by_param(event, param_name) -> Tuple[dict, int]:
     if not origin_url:
         return ({"message": "No origin url"}, 404)
 
-    redis.extend_expire(origin_url, days=7)
-    redis.extend_expire(short_id, days=7)
+    redis.extend_expire(origin_url, ttl=timedelta(days=7))
+    redis.extend_expire(short_id, ttl=timedelta(days=7))
 
     body = {
         "short_id": short_id,
